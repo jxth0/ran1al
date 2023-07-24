3902ca730cc1fe535312e3d450c9299482a4fea6
jxth0 <116193830+jxth0@users.noreply.github.com>
2023-07-24 20:02:33 +0300

    Add files via upload

---
+++ date.py
@@ -0,0 +1,16 @@
+chose=int(input('2 & * = 1 , 1 & * = 2'))
+
+if chose == 1:
+    
+    z=int(input("ENTER NUMBER :"))
+
+    for z in range (z):
+        
+        print(z*2)
+
+elif chose == 2:
+    z=int(input("ENTER NUMBER :"))
+
+    for z in range (z):
+        
+        print(1+z*2)
