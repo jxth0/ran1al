dbb934d0bdac171c8aea000e6e357c7b6bdf27d1
jxth0 <116193830+jxth0@users.noreply.github.com>
2023-07-24 18:50:58 +0300

    Add files via upload

---
+++ gcub.py
@@ -0,0 +1,19 @@
+FULLNAME=input("ENTER FIRST NAME : ")
+
+match FULLNAME.upper():
+    case 'ALI':
+        print(" FULLNAME : Ali Huusin ")
+
+    case 'HAMZA':
+        print("FULLNAME : Hamza Al-Attas")
+
+    case 'SALIM':
+        print("FULLNAME : Salem Al-Attas")
+
+    case 'ABDULLAH':
+        print("FULLNAME : Abdullah Baaqil")
+
+    case OTHER :
+        print("ERORR NAME !")
+
+    
