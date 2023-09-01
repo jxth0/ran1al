0166a2d5f5cfa3944ffa558e467dbc9a6a04aac8
jxth0 <116193830+jxth0@users.noreply.github.com>
2023-09-01 07:10:15 +0300

    Add files via upload

---
+++ DEF TEST 1.py
@@ -0,0 +1,31 @@
+#def evail1():
+#    a=eval(input('ENTER NUMBER:'))
+#    print("NUMBER IS :" + str(a))
+#evail1()
+
+#----------------------------------------------------------------------------------
+
+#def aj(**info):
+#    print(info)
+#aj(firstname = 'Ali', lastname= 'Huusin')
+
+#Note: ** = 'Dictionary' 
+
+
+#----------------------------------------------------------------------------------
+
+#def my_relatives_name():
+#    print('HI MY NAME IS ALI HUUSIN MY RELATIVES :')
+    
+    
+#    print("humza ")
+#    print("salim")
+#    print("Abdullah")   
+#    print("Abdullah Taher")    
+#    print("Hamza Al-Attas")
+#my_relatives_name()
+
+
+
+
+
