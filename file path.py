48ff98e6f27dca0da92ef7439d1c9b1c986c3ca6
jxth0 <116193830+jxth0@users.noreply.github.com>
2023-11-25 17:10:38 +0300

    Add files via upload

---
+++ file path.py
@@ -0,0 +1,20 @@
+# For New File
+# input1=input('Enter Anything :')
+# namefile=input('Name File :')
+# file_path=f'C:\\{namefile}'
+
+# with open (file_path,'x') as file :
+#     file.write(input1)
+
+
+# --------------------------------------------------------------
+
+
+# For Open File
+# namefile=input('Name File :')
+# namefileandtxt=namefile+'.txt'
+# file_path=f'C:\\{namefileandtxt}'
+
+# with open (file_path,'r') as file :
+#     text=file.read()
+#     print(text)
